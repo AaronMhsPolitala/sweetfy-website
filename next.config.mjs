9b92f4923989f4bb4bd355cda77185a9bcc686be
AronNHW <aaliyahpanggung@gmail.com>
2026-08-29 12:09:10 +0800

    Configure static HTML export for Netlify hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  outputFileTracing: false,
   images: {
     unoptimized: true,
     remotePatterns: [
